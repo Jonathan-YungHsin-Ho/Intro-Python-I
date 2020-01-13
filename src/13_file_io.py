8f10bd1956a3308498c434a294391b976e63cf45
Jonathan Ho <johnnyho82@gmail.com>
2020-01-13 18:51:10 -0500

    Finish 13_file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+# f = open('foo.txt', 'r')
+# print(f.read())
+# f.close()
+
+with open('foo.txt') as f:
+    print(f.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +22,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+# f = open('bar.txt', 'w')
+# f.write('O snail\n')
+# f.write('Climb Mount Fuji,\n')
+# f.write('But slowly, slowly!\n')
+# f.close()
+
+with open('bar.txt', 'w') as f:
+    f.write('O snail\n')
+    f.write('Climb Mount Fuji,\n')
+    f.write('But slowly, slowly!\n')
